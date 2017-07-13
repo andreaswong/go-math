ad1d955757d0c35d97cbcbd06af50fd51377e505
Andreas Wong <andreaswong770@gmail.com>
2017-07-13 23:54:19 +0800

    Master: added even/odd logic

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+	"strconv"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "gogogo", nil)
-	})
-
 	http.HandleFunc("/api/math/even", func(w http.ResponseWriter, r *http.Request) {
+		numberStr := r.URL.Query().Get("number")
 
+		if len(numberStr) > 0 {
+			if number, err := strconv.Atoi(numberStr); err == nil {
+				if number%2 == 0 {
+					fmt.Fprintf(w, "true")
+				} else {
+					fmt.Fprintf(w, "false")
+				}
+			} else {
+				fmt.Fprintf(w, numberStr + " is not a number")
+			}
+		}
 	})
 
-	http.ListenAndServe(":80", nil)
+	fmt.Println(http.ListenAndServe(":8889", nil))
 }
